5a99a927c1bcfea67f57a1e9db3f4137fe793e07
mateus.alves <mateus.alves@locaweb.com.br>
2015-03-25 18:37:20 -0300

    teste com excecao

--- aula3/spec/calculadora_spec.rb
+++ aula3/spec/calculadora_spec.rb
@@ -1,60 +1,151 @@
 require_relative '../calculadora'
 
 RSpec.describe Calculadora do
+	let(:calc) { Calculadora.new }
+	let(:a) { 5 }
+	let(:b) { 5 }
 
 	describe "#soma" do
-		it "Test the valid value of a sum" do
-					
-			result = subject.soma(5,5)
-			expect(result).to eql(10)
+		subject { calc.soma(a,b) }
+		
+		context "soma numeros inteiros" do
+			it "should be 10" do
+				# subject.should eq 10
+				expect(subject).to eq 10
+			end
 		end
 		
-		it "Test the invalid value of a sum" do
-					
-			result = subject.soma(5,3)
-			expect(result).to_not eql(10)
+		context "soma numeros com b negativo" do
+			let(:b) { -1 }
+
+			it "should be 4" do
+				# subject.should eq 4
+				expect(subject).to eq 4
+			end
+		end
+
+		context "soma numero invalido" do
+			let(:a) { "a" }
+			
+			it "when a is a string" do
+				#expect { subject }.to raise_error(ArgumentError, "Invalid parameters")
+				expect { subject }.to raise_error(Exception)
+			end
+
+		end
+
+		context "resultado da soma inválido" do
+			let(:b) { 3 }	
+			it "não deve ser igual a 10" do	
+				expect(subject).to_not eql 10
+			end
 		end
 	end
- 
+
 	describe "#divide" do
-		it "Test the valid value of a division" do
-					
-			result = subject.divide(9,3)
-			expect(result).to eql(3)
-		end
+		subject { calc.divide(a,b) }
+		let(:a) { 9 }
+
+		context "dividir numeros inteiros" do
+			let(:b) { 3 }
+
+			it "divisão válida" do				
+				expect(subject).to eql(3)
+			end
 		
-		it "Test the invalid value of a division" do
-					
-			result = subject.divide(15,3)
-			expect(result).to_not eql(3)
+			it "não retornar o valor exato" do
+				expect(subject).to_not eql(5)
+			end
 		end
+
+		context "divisão por zero" do
+			let(:b) { 0 }
+
+			it "divisão por zero" do				
+				expect { subject }.to raise_error(Exception)
+			end
+		end
+
+		context "divisao numero invalido" do
+			let(:a) { "a" }
+			
+			it "when a is a string" do
+				#expect { subject }.to raise_error(ArgumentError, "Invalid parameters")
+				expect { subject }.to raise_error(Exception)
+			end
+		end
+
 	end
- 
+
 	describe "#multiplica" do
-		it "Test the valid value of a multiplication" do
-					
-			result = subject.multiplica(9,3)
-			expect(result).to eql(27)
+		subject { calc.multiplica(a,b) }
+		
+		context "multiplicacao numeros inteiros" do
+			it "should be 10" do
+				# subject.should eq 10
+				expect(subject).to eq 25
+			end
 		end
 		
-		it "Test the invalid value of a multiplication" do
-					
-			result = subject.multiplica(15,3)
-			expect(result).to_not eql(3)
+		context "multiplicacao numeros com b negativo" do
+			let(:b) { -1 }
+
+			it "should be -5" do
+				# subject.should eq -5
+				expect(subject).to eq -5
+			end
+		end
+
+		context "multiplicacao numero invalido" do
+			let(:a) { "a" }
+			
+			it "when a is a string" do
+				#expect { subject }.to raise_error(ArgumentError, "Invalid parameters")
+				expect { subject }.to_not raise_error(Exception)
+			end
+		end
+
+		context "resultado da multiplicacao inválido" do
+			let(:b) { 3 }	
+			it "não deve ser igual a 10" do	
+				expect(subject).to_not eql 10
+			end
 		end
 	end
- 
+
 	describe "#subtrai" do
-		it "Test the valid value of a subtraction" do
-					
-			result = subject.subtrai(9,3)
-			expect(result).to eql(6)
+		subject { calc.subtrai(a,b) }
+		
+		context "subtracao numeros inteiros" do
+			it "should be 10" do
+				# subject.should eq 10
+				expect(subject).to eq 0
+			end
 		end
 		
-		it "Test the invalid value of a subtraction" do
-					
-			result = subject.subtrai(15,3)
-			expect(result).to_not eql(3)
+		context "subtracao numeros com b negativo" do
+			let(:b) { -1 }
+
+			it "should be 4" do
+				# subject.should eq 6
+				expect(subject).to eq 6
+			end
+		end
+
+		context "subtracao numero invalido" do
+			let(:a) { "a" }
+			
+			it "when a is a string" do
+				#expect { subject }.to raise_error(ArgumentError, "Invalid parameters")
+				expect { subject }.to raise_error(Exception)
+			end
+		end
+
+		context "resultado da subtracao inválido" do
+			let(:b) { 3 }	
+			it "não deve ser igual a 10" do	
+				expect(subject).to_not eql 10
+			end
 		end
 	end
 
