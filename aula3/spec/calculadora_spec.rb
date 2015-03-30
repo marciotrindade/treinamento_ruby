2f2dcbe6f364a9bd2117b924bb94289d19037a66
Robson Agapito <robson.agapito@locaweb.com.br>
2015-03-30 09:56:37 -0300

    ajustando testes unitários da calculadora

--- aula3/spec/calculadora_spec.rb
+++ aula3/spec/calculadora_spec.rb
@@ -1,21 +1,32 @@
 require_relative '../calculadora'
 
 RSpec.describe Calculadora do 
-  describe Calculadora do
-    it '#soma' do
+  describe '#soma' do
+    it 'sum two numbers' do
       expect(subject.soma 5,2).to eql 7 
     end 
-    it '#divide' do
+  end
+
+  describe '#divide' do
+    it 'division of two numbers ' do
       expect(subject.divide 9,3).to eql 3
     end 
-    it '#multiplica' do
-      expect(subject.multiplica 4,6).to eql 24
-    end 
-    it '#subtrai' do
-      expect(subject.subtrai 7,3).to eql 4
-    end 
-    it "div by zero" do
+
+    it 'division of two numbers and divisor equal zero' do
       expect{subject.divide 4,0}.to raise_error(ZeroDivisionError)
     end
-  end  
+  end
+
+  describe '#multiplica' do
+    it 'multiplication of two numbers' do
+      expect(subject.multiplica 4,6).to eql 24
+    end 
+  end
+
+  describe '#subtrai' do
+    it 'subtraction of two numbers' do
+      expect(subject.subtrai 7,3).to eql 4
+    end 
+  end
+
 end
