50bdfed9a12cf840e9f8a065fe0945c00f9d6a91
Marcelo Toledo - Nitrous <marcelotoledo5000@gmail.com>
2015-05-11 14:32:50 +0000

    Include new actions to Post: New, Update, Delete

--- aula6/blog/app/models/post.rb
+++ aula6/blog/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  validates :title, :content, :user_id, presence: true
 end
--- aula6/blog/config/routes.rb
+++ aula6/blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  resources :posts, only: [:index, :show, :new, :edit]  # or except [:create, :update, :delete]
+ #resources :posts, only: [:index, :show, :new, :edit, :create]  # or except [:update, :delete]
+  resources :posts
   root:to => 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
