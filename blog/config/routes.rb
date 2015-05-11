12fb737ccc6bb3b4efe4dd782d83ea715e3502b6
mvvamo <mvvamo@gmail.com>
2015-05-11 02:27:58 +0000

    Listando blogs e o titulo na url

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   #get 'users' => 'user#index'
   resources :users
+  
+  #get "posts" , "posts#index"
+  #get 'posts/:id'
+  
+  resources :posts, only: [:index, :show]
+    
+  
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
